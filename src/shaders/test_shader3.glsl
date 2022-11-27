850f6c777d03a7d4e8c5ae46346eebe837fc6e34
Grzegorz Kwiatkowski <grzegorz.kwiatkowski41@gmail.com>
2022-11-27 21:09:51 +0100

    buffer swap

---
+++ src/shaders/test_shader3.glsl
@@ -0,0 +1,16 @@
+#version 450
+
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+layout(set = 0, binding = 0) buffer DataIn {
+    uint data[];
+} dataIn;
+
+layout(set = 0, binding = 1) buffer DataOut {
+    uint data[];
+} dataOut;
+
+void main() {
+    uint idx = gl_GlobalInvocationID.x;
+    dataOut.data[idx] = dataIn.data[idx] + 1;
+}
--- src/solver_gpu.rs
+++ src/solver_gpu.rs
@@ -13,9 +13,7 @@ use vulkano::{
 
 use crate::{gpu_utils::{init, get_compute_pipeline}, gol_instance::{self, GolInstance}};
 
-// const VALUE_MUL: u32 = 14;
-
-const RUN_COUNT: u32 = 10000;
+const RUN_COUNT: u32 = 5;
 
 // fn gol_to_buff(gol: &GolInstance) -> Vec<i32> {
 //     let cells = gol.cells;
@@ -26,7 +24,7 @@ pub fn run_gpu() {
     mod cs {
         vulkano_shaders::shader! {
             ty: "compute",
-            path: "./src/shaders/test_shader.glsl",
+            path: "./src/shaders/test_shader3.glsl",
             // types_meta: {
             //     use bytemuck::{Pod, Zeroable};
     
@@ -43,11 +41,22 @@ pub fn run_gpu() {
     let command_buffer_allocator =
         StandardCommandBufferAllocator::new(device.clone(), Default::default());
 
-    // We start by creating the buffer that will store the data.
     let data_buffer = {
-        // Iterator that produces the data.
         let data_iter = 0..65536u32;
-        // Builds the buffer and fills it with this iterator.
+        CpuAccessibleBuffer::from_iter(
+            &memory_allocator,
+            BufferUsage {
+                storage_buffer: true,
+                ..BufferUsage::empty()
+            },
+            false,
+            data_iter,
+        )
+        .unwrap()
+    };
+
+    let data_buffer2 = {
+        let data_iter = 0..65536u32;
         CpuAccessibleBuffer::from_iter(
             &memory_allocator,
             BufferUsage {
@@ -66,6 +75,17 @@ pub fn run_gpu() {
         layout.clone(),
         [
             WriteDescriptorSet::buffer(0, data_buffer.clone()),
+            WriteDescriptorSet::buffer(1, data_buffer2.clone()),
+        ],
+    )
+    .unwrap();
+
+    let set_reverse = PersistentDescriptorSet::new(
+        &descriptor_set_allocator,
+        layout.clone(),
+        [
+            WriteDescriptorSet::buffer(1, data_buffer.clone()),
+            WriteDescriptorSet::buffer(0, data_buffer2.clone()),
         ],
     )
     .unwrap();
@@ -74,7 +94,6 @@ pub fn run_gpu() {
     //     val: VALUE_MUL
     // };
 
-    // In order to execute our operation, we have to build a command buffer.
     let mut builder = AutoCommandBufferBuilder::primary(
         &command_buffer_allocator,
         queue.queue_family_index(),
@@ -92,46 +111,38 @@ pub fn run_gpu() {
     
     for _i in 0..RUN_COUNT {
         builder
+            .bind_descriptor_sets( // default descriptors
+                PipelineBindPoint::Compute,
+                pipeline.layout().clone(),
+                0,
+                set.clone(),
+            )
+            .dispatch([1024, 1, 1])
+            .unwrap()
+            .bind_descriptor_sets( // reversed descriptors
+                PipelineBindPoint::Compute,
+                pipeline.layout().clone(),
+                0,
+                set_reverse.clone(),
+            )
             .dispatch([1024, 1, 1])
             .unwrap();
     }
-        
-    // Finish building the command buffer by calling `build`.
+    
     let command_buffer = builder.build().unwrap();
 
-    // Let's execute this command buffer now.
-    // To do so, we TODO: this is a bit clumsy, probably needs a shortcut
     let future = sync::now(device.clone())
         .then_execute(queue.clone(), command_buffer)
         .unwrap()
-        // This line instructs the GPU to signal a *fence* once the command buffer has finished
-        // execution. A fence is a Vulkan object that allows the CPU to know when the GPU has
-        // reached a certain point.
-        // We need to signal a fence here because below we want to block the CPU until the GPU has
-        // reached that point in the execution.
         .then_signal_fence_and_flush()
         .unwrap();
 
-    // Blocks execution until the GPU has finished the operation. This method only exists on the
-    // future that corresponds to a signalled fence. In other words, this method wouldn't be
-    // available if we didn't call `.then_signal_fence_and_flush()` earlier.
-    // The `None` parameter is an optional timeout.
-    //
-    // Note however that dropping the `future` variable (with `drop(future)` for example) would
-    // block execution as well, and this would be the case even if we didn't call
-    // `.then_signal_fence_and_flush()`.
-    // Therefore the actual point of calling `.then_signal_fence_and_flush()` and `.wait()` is to
-    // make things more explicit. In the future, if the Rust language gets linear types vulkano may
-    // get modified so that only fence-signalled futures can get destroyed like this.
     future.wait(None).unwrap();
 
-    // Now that the GPU is done, the content of the buffer should have been modified. Let's
-    // check it out.
-    // The call to `read()` would return an error if the buffer was still in use by the GPU.
     let data_buffer_content = data_buffer.read().unwrap();
     for n in 0..65536u32 {
-        assert_eq!(data_buffer_content[n as usize], n + RUN_COUNT);
+        assert_eq!(data_buffer_content[n as usize], n + (2 * RUN_COUNT));
     }
 
-    println!("Success 1");
+    println!("Success");
 }
