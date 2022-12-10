0439d9d2fbe8378e903df12eeec077daf38ed00b
Grzegorz Kwiatkowski <g.kwiatkowski@millionmonkeyssoftware.com>
2022-12-10 03:03:55 +0100

    data collection

--- bencher/benchmark.py
+++ bencher/benchmark.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 import subprocess
@@ -14,10 +15,11 @@ FNULL = open(os.devnull, 'w')
 MIN_X = 10
 MIN_T = 10
 
-MAX_X = 10000
-MAX_T = 10000
+MAX_X = MIN_X + 10000
+MAX_T = MIN_T + 10000
 
-STEP = 500
+STEP_X = 500
+STEP_T = 500
 
 CPU = 'CPU'
 GPU = 'GPU'
@@ -25,15 +27,33 @@ GPU = 'GPU'
 RAW = 'raw'
 MEAN = 'mean'
 
-N = 20
-SOFT_LIMIT = 3
-HARD_LIMIT = 10
+N = 10              # nr of samples 
+SOFT_LIMIT = 3      # those params are big enough, don't go higher
+HARD_LIMIT = 5      # that's too long, don't bother for now
+
+# Overrides ---------
+
+MIN_X = 15
+STEP_X = 5
+MAX_X = MIN_X + 45
+SOFT_LIMIT = 2
+HARD_LIMIT = 4
+STEP_T = 200
+MIN_T = 6000
+
+# -------------------
+
+
 
 def test(x, t, mode):
     line = f'{gol_path} {x} {x} {t} {mode}'
-    t = time.time()
-    subprocess.call(line, stdout=FNULL, stderr=FNULL, shell=False)
-    return time.time() - t
+    try:
+        t = time.time()
+        subprocess.call(line, stdout=FNULL, stderr=FNULL, shell=False, timeout=HARD_LIMIT)
+        return time.time() - t
+    except subprocess.TimeoutExpired:
+        return HARD_LIMIT
+        
 
 def propsPrefix(x, t, mode):
     return f'{x},{t},{mode}'
@@ -46,6 +66,8 @@ def test_mean(props, fRaw, fMean):
 
     for i in range(N):
         t = test(*props)
+        if (t >= HARD_LIMIT): 
+            return t
         m += t
         low = min(low, t)
         high = max(high, t)
@@ -56,16 +78,33 @@ def test_mean(props, fRaw, fMean):
     return m
 
 
-sizes = np.arange(MIN_X, MAX_X, STEP)
-times = np.arange(MIN_T, MAX_T, STEP)
+sizes = np.arange(MIN_X, MAX_X, STEP_X)
+times = np.arange(MIN_T, MAX_T, STEP_T)
 
 if __name__ == '__main__':
     now = datetime.now()
     timestamp = now.strftime("%m-%d-%Y_%H-%M-%S")
-    fRaw = open('logs/' + timestamp + '-raw.csv', "w")
+    os.mkdir('logs/' + timestamp)
+    fRaw = open('logs/' + timestamp + '/raw.csv', "w")
     fRaw.write(log_header_raw)
-    fMean = open('logs/' + timestamp + '-mean.csv', "w")
+    fMean = open('logs/' + timestamp + '/mean.csv', "w")
     fMean.write(log_header_mean)
+
+    with open('logs/' + timestamp + '/env.json', "w") as fJson:
+        json.dump({
+            'MIN_X': MIN_X,
+            'MIN_T': MIN_T,
+
+            'MAX_X': MAX_X,
+            'MAX_T': MAX_T,
+
+            'STEP_X': STEP_X,
+            'STEP_T': STEP_T,
+
+            'N': N,
+            'SOFT_LIMIT': SOFT_LIMIT,
+            'HARD_LIMIT': HARD_LIMIT 
+        }, fJson, indent=4)
 
     sc = len(sizes)
     tc = len(times)
@@ -75,10 +114,12 @@ if __name__ == '__main__':
         t = times[i % tc]
         if (x == limit_x):
             continue
-        params = (x, t, GPU)
+        params = (x, t, CPU)
         mn = test_mean(params, fRaw, fMean)
         if (mn > SOFT_LIMIT):
             limit_x = x
+            if (i % tc == 0): # first in row was too long
+                break
 
     fRaw.close()
     fMean.close()
---
+++ vis/ploter.py
@@ -0,0 +1,143 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# file = 'data/small-CPU-raw.csv'
+file = 'data/meanGPU.csv'
+file2 = 'data/mean.csv'
+
+means_types = {
+    'SIZE': np.int64,
+    'DURATION': np.int64,
+    'MODE': str,
+    'TIME': np.float64,
+    'MIN': np.float64,
+    'MAX': np.float64,
+    'N': np.int64
+}
+
+def plotBoundary():
+    data = []
+    colors = []
+    with open(file, 'r') as f:
+        for line in f.readlines()[1:]:
+            parts = line.split(',')
+            data.append([float(parts[0]), float(parts[1]), float(parts[3])])
+            colors.append('blue' if float(parts[3]) < 3 else 'red')
+    data = np.array(data)
+
+    fig, axs = plt.subplots(1, 2)
+
+    ax = axs[0]
+    ax.scatter(data[:, 0], data[:, 1], c=colors, marker='x')
+    ax.set_xlabel('size (side length L)')
+    ax.set_ylabel('duration')
+    
+
+    ax = axs[1]
+    ax.scatter(data[:, 0] * data[:, 0] * data[:, 1], data[:, 2], c=colors, marker='o')
+    ax.set_xlabel('size * duration (L^2 * T)')
+    ax.set_ylabel('running time')
+
+    plt.show()
+
+
+def plotError():
+    # data = []
+    # colors = []
+    df = pd.read_csv(file2, dtype=means_types)
+
+    fig, axs = plt.subplots(1, 2)
+    # fig.suptitle('CPU simulations of GOL (mean of 10) on AMD Radeon RX 6600 XT')
+    fig.suptitle('CPU simulations of GOL (mean of 10) on AMD Ryzen 5 3600')
+
+    ax = axs[0]
+    m = ax.scatter(df['SIZE'], df['DURATION'], marker='x', vmin=0, vmax=1, c=df['TIME'], cmap='jet')
+    ax.set_xlabel('size (side length L)')
+    ax.set_ylabel('duration')
+    fig.colorbar(m, cmap='jet')
+    
+
+    ax = axs[1]
+    ax.set_xlabel('duration')
+    ax.set_ylabel('running time (s)')
+
+    def plotErrorAx(n):
+        dfn = df[df['SIZE'] == n]
+        err = [
+            dfn['TIME'] - dfn['MIN'],
+            dfn['MAX'] - dfn['TIME']
+        ]
+        ax.errorbar(dfn['DURATION'], dfn['TIME'], yerr=err, label=f'size = {n}')
+    
+    # plotErrorAx(100)
+    # plotErrorAx(500)
+    # plotErrorAx(1000)
+    # plotErrorAx(1500)
+    # plotErrorAx(2000)
+    plotErrorAx(10)
+    plotErrorAx(40)
+    plotErrorAx(50)
+    plotErrorAx(60)
+    plotErrorAx(100)
+    plotErrorAx(30)
+    plotErrorAx(80)
+    
+    ax.legend()
+
+    plt.show()
+
+def plotComp():
+    df = pd.read_csv(file, dtype=means_types)
+    df2 = pd.read_csv(file2, dtype=means_types)
+
+    fig, axs = plt.subplots(1, 2)
+    # fig.suptitle('CPU simulations of GOL (mean of 10) on AMD Radeon RX 6600 XT')
+    fig.suptitle('Simulations of GOL CPU vs GPU (mean of 10) on AMD Ryzen 5 3600 and AMD Radeon RX 6600 XT')
+
+    ax = axs[0]
+    m = ax.scatter(df['SIZE'], df['DURATION'], marker='+', vmin=0, vmax=1, c=df['TIME'], cmap='jet', label="GPU")
+    m = ax.scatter(df2['SIZE'], df2['DURATION'], marker='x', vmin=0, vmax=1, c=df2['TIME'], cmap='jet', label="CPU")
+    ax.set_xlabel('size (side length L)')
+    ax.set_ylabel('duration')
+    ax.legend();
+    fig.colorbar(m, cmap='jet')
+    
+
+    ax = axs[1]
+    ax.set_xlabel('duration')
+    ax.set_ylabel('running time (s)')
+
+    def plotErrorAx(mode, n):
+        dfn = None
+        if (mode == 'CPU'):
+            dfn = df2[df2['SIZE'] == n]
+        else:
+            dfn = df[df['SIZE'] == n]
+        err = [
+            dfn['TIME'] - dfn['MIN'],
+            dfn['MAX'] - dfn['TIME']
+        ]
+        ax.errorbar(dfn['DURATION'], dfn['TIME'], yerr=err, label=f'size = {n} ({mode})')
+    
+    plotErrorAx('GPU', 100)
+    # plotErrorAx('GPU', 500)
+    plotErrorAx('GPU', 1000)
+    # plotErrorAx('GPU', 1500)
+    plotErrorAx('GPU', 2000)
+    # plotErrorAx(10)
+    plotErrorAx('CPU', 40)
+    # plotErrorAx('CPU', 60)
+    # plotErrorAx('CPU', 80)
+    plotErrorAx('CPU', 100)
+    # plotErrorAx(50)
+    # plotErrorAx(30)
+    
+    ax.legend()
+
+    plt.show()
+
+if __name__ == '__main__':
+    # plotBoundary()
+    plotError()
+    # plotComp()
